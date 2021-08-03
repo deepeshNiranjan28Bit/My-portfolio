0668e05faf278558ce130a9cd8a83f9c20823f6c
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-08-03 23:41:44 +0530

    Updates

--- static/js/main.830c86bd.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-port"]=this["webpackJsonpreact-port"]||[]).push([[0],{9:function(t,e,n){"use strict";n.r(e);var c=n(1),r=n.n(c),i=n(3),o=n.n(i),s=n(0);var a=function(){return Object(s.jsx)("div",{children:"hey harsh"})},h=function(t){t&&t instanceof Function&&n.e(3).then(n.bind(null,10)).then((function(e){var n=e.getCLS,c=e.getFID,r=e.getFCP,i=e.getLCP,o=e.getTTFB;n(t),c(t),r(t),i(t),o(t)}))};o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root")),h()}},[[9,1,2]]]);
-//# sourceMappingURL=main.830c86bd.chunk.js.map
